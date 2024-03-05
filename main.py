639473e86dd578f7b93bc0cc8d83eca3a670fedb
kimharry <kimharry0610@gmail.com>
2024-03-05 02:50:43 +0000

    before adapting mia, just vae

---
+++ main.py
@@ -0,0 +1,97 @@
+from __future__ import print_function
+
+from torch.autograd import Variable
+
+import torch.backends.cudnn as cudnn
+import torch.nn.init as init
+import torch.utils.data
+
+from data_loaders.cifar10_data_loader import CIFAR10DataLoader
+from loss import Loss
+from model import VAE
+from trainer import Trainer
+from utils.utils import *
+from utils.weight_initializer import Initializer
+
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+def plot_images(original_images, reconstructed_images, title, filename):
+    num_images = original_images.shape[0]
+    num_comparisons = len(reconstructed_images)
+    fig, axs = plt.subplots(num_comparisons+1, num_images, figsize=(num_images*num_comparisons+1, 4))
+
+    image_shape = (3, 32, 32)
+
+    for i in range(num_images):
+        axs[0, i].imshow(np.clip(np.transpose(original_images[i].reshape(image_shape), (1, 2, 0)), 0, 1))
+        axs[0, i].axis('off')
+        for j in range(1, num_comparisons+1):
+            axs[j, i].imshow(np.clip(np.transpose(reconstructed_images[j-1][i].reshape(image_shape), (1, 2, 0)), 0, 1))
+            axs[j, i].axis('off')
+
+    fig.suptitle(title)
+
+    now = datetime.now() 
+    current_time = now.strftime("%Y-%m-%d_%H-%M-%S")
+    filename_with_time = f"{filename}_{current_time}.png"
+
+    plt.savefig(filename_with_time)
+    plt.show()
+
+
+def main():
+    # Parse the JSON arguments
+    args = parse_args()
+
+    # Create the experiment directories
+    args.summary_dir, args.checkpoint_dir = create_experiment_dirs(
+        args.experiment_dir)
+
+    model = VAE()
+
+    # to apply xavier_uniform:
+    Initializer.initialize(model=model, initialization=init.xavier_uniform, gain=init.calculate_gain('relu'))
+
+    loss = Loss()
+
+    if torch.cuda.is_available():
+        model.cuda()
+        loss.cuda()
+        cudnn.enabled = True
+        cudnn.benchmark = True
+
+    print("Loading Data...")
+    data = CIFAR10DataLoader(args)
+    print("Data loaded successfully\n")
+
+    trainer = Trainer(model, loss, data.train_loader, data.test_loader, args)
+
+    if args.to_train:
+        try:
+            print("Training...")
+            trainer.train()
+            print("Training Finished\n")
+        except KeyboardInterrupt:
+            print("Training had been Interrupted\n")
+
+    if args.to_test:
+        print("Testing on training data...")
+        trainer.test_on_trainings_set()
+        print("Testing Finished\n")
+
+    if args.generate_result:
+        temp_data, _ = next(iter(data.test_loader))
+        if args.cuda:
+            temp_data = temp_data.cuda()
+        temp_data = Variable(temp_data)
+        [outputs, _, _] = model(temp_data)
+        outputs = outputs.view(-1, 3, 32, 32)
+        outputs = outputs.detach().cpu().numpy()
+
+        plot_images(temp_data.detach().cpu().numpy(), [outputs], 'VanillaVAE', 'VanillaVAE')
+
+
+if __name__ == "__main__":
+    main()
---
+++ trainer.py
@@ -0,0 +1,136 @@
+from torch.autograd import Variable
+from torch import optim
+import torch
+
+from tensorboardX import SummaryWriter
+import shutil
+from tqdm import tqdm
+import numpy as np
+
+
+class Trainer:
+    def __init__(self, model, loss, train_loader, test_loader, args):
+        self.model = model
+        self.args = args
+        self.args.start_epoch = 0
+
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+
+        # Loss function and Optimizer
+        self.loss = loss
+        self.optimizer = self.get_optimizer()
+
+        # Tensorboard Writer
+        self.summary_writer = SummaryWriter(log_dir=args.summary_dir)
+        # Model Loading
+        if args.resume:
+            self.load_checkpoint(self.args.resume_from)
+
+    def train(self):
+        self.model.train()
+        for epoch in range(self.args.start_epoch, self.args.num_epochs):
+            loss_list = []
+            print("epoch {}...".format(epoch))
+            for batch_idx, (data, _) in enumerate(tqdm(self.train_loader)):
+                if self.args.cuda:
+                    data = data.cuda()
+                data = Variable(data)
+                self.optimizer.zero_grad()
+                recon_batch, mu, logvar = self.model(data)
+                loss = self.loss(recon_batch, data, mu, logvar)
+                loss.backward()
+                self.optimizer.step()
+                loss_list.append(loss.item())
+
+            print("epoch {}: - loss: {}".format(epoch, np.mean(loss_list)))
+            new_lr = self.adjust_learning_rate(epoch)
+            print('learning rate:', new_lr)
+
+            self.summary_writer.add_scalar('training/loss', np.mean(loss_list), epoch)
+            self.summary_writer.add_scalar('training/learning_rate', new_lr, epoch)
+            self.save_checkpoint({
+                'epoch': epoch + 1,
+                'state_dict': self.model.state_dict(),
+                'optimizer': self.optimizer.state_dict(),
+            })
+            if epoch % self.args.test_every == 0:
+                self.test(epoch)
+
+    def test(self, cur_epoch):
+        print('testing...')
+        self.model.eval()
+        test_loss = 0
+        for i, (data, _) in enumerate(self.test_loader):
+            if self.args.cuda:
+                data = data.cuda()
+            data = Variable(data, volatile=True)
+            recon_batch, mu, logvar = self.model(data)
+            test_loss += self.loss(recon_batch, data, mu, logvar).item()
+            # if i == 0:
+            #     n = min(data.size(0), 8)
+            #     comparison = torch.cat([data[:n],
+            #                             recon_batch.view(-1, 3, 32, 32)[:n]])
+            #     self.summary_writer.add_image('testing_set/image', comparison, cur_epoch)
+
+        test_loss /= len(self.test_loader.dataset)
+        print('====> Test set loss: {:.4f}'.format(test_loss))
+        self.summary_writer.add_scalar('testing/loss', test_loss, cur_epoch)
+        self.model.train()
+
+    def test_on_trainings_set(self):
+        print('testing...')
+        self.model.eval()
+        test_loss = 0
+        for i, (data, _) in enumerate(self.train_loader):
+            if self.args.cuda:
+                data = data.cuda()
+            data = Variable(data, volatile=True)
+            recon_batch, mu, logvar = self.model(data)
+            test_loss += self.loss(recon_batch, data, mu, logvar).item()
+            # if i % 50 == 0:
+            #     n = min(data.size(0), 8)
+            #     comparison = torch.cat([data[:n],
+            #                             recon_batch.view(-1, 3, 32, 32)[:n]])
+            #     self.summary_writer.add_image('training_set/image', comparison, i)
+
+        test_loss /= len(self.test_loader.dataset)
+        print('====> Test on training set loss: {:.4f}'.format(test_loss))
+        self.model.train()
+
+    def get_optimizer(self):
+        return optim.Adam(self.model.parameters(), lr=self.args.learning_rate,
+                          weight_decay=self.args.weight_decay)
+
+    def adjust_learning_rate(self, epoch):
+        """Sets the learning rate to the initial LR multiplied by 0.98 every epoch"""
+        learning_rate = self.args.learning_rate * (self.args.learning_rate_decay ** epoch)
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = learning_rate
+        return learning_rate
+
+    def save_checkpoint(self, state, is_best=False, filename='checkpoint.pth.tar'):
+        '''
+        a function to save checkpoint of the training
+        :param state: {'epoch': cur_epoch + 1, 'state_dict': self.model.state_dict(),
+                            'optimizer': self.optimizer.state_dict()}
+        :param is_best: boolean to save the checkpoint aside if it has the best score so far
+        :param filename: the name of the saved file
+        '''
+        torch.save(state, self.args.checkpoint_dir + filename)
+        if is_best:
+            shutil.copyfile(self.args.checkpoint_dir + filename,
+                            self.args.checkpoint_dir + 'model_best.pth.tar')
+
+    def load_checkpoint(self, filename):
+        filename = self.args.checkpoint_dir + filename
+        try:
+            print("Loading checkpoint '{}'".format(filename))
+            checkpoint = torch.load(filename)
+            self.args.start_epoch = checkpoint['epoch']
+            self.model.load_state_dict(checkpoint['state_dict'])
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
+            print("Checkpoint loaded successfully from '{}' at (epoch {})\n"
+                  .format(self.args.checkpoint_dir, checkpoint['epoch']))
+        except:
+            print("No checkpoint exists from '{}'. Skipping...\n".format(self.args.checkpoint_dir))
