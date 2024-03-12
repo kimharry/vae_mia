2a37e8bf18c2f7a801bd6bb7e93890802e0aec08
kimharry <kimharry0610@kentech.ac.kr>
2024-03-12 03:08:00 +0000

    resnet32_split_trainer.py total model train testing

--- resnet32_split_trainer.py
+++ resnet32_split_trainer.py
@@ -21,7 +21,7 @@ parser.add_argument('--batch_size', default=128, help='')
 parser.add_argument('--batch_size_test', default=100, help='')
 parser.add_argument('--num_worker', default=4, help='')
 parser.add_argument('--logdir', type=str, default='logs', help='')
-parser.add_argument('--num_epochs', default=200, help='')
+parser.add_argument('--num_epochs', default=500, help='')
 args = parser.parse_args()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -38,9 +38,9 @@ transforms_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-dataset_train = CIFAR10(root='../data', train=True, 
+dataset_train = CIFAR10(root='data', train=True, 
                         download=True, transform=transforms_train)
-dataset_test = CIFAR10(root='../data', train=False, 
+dataset_test = CIFAR10(root='data', train=False, 
                         download=True, transform=transforms_test)
 train_loader = DataLoader(dataset_train, batch_size=args.batch_size, 
                           shuffle=True, num_workers=args.num_worker)
@@ -61,7 +61,7 @@ if args.resume is not None:
     net.load_state_dict(checkpoint['net'])
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
+optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-4)
 
 decay_epoch = [50, 100, 150]
 step_lr_scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=decay_epoch, gamma=0.1)
