0c72f37ba8a46f4f7c8774a417f1bdb5e0714e5d
Harry Kim <harry@Harryui-noteubug.local>
2024-03-19 21:28:00 +0900

    dataset split mechanism modified to divided equally per classes

--- resnet32_split_trainer.py
+++ resnet32_split_trainer.py
@@ -14,7 +14,7 @@ from torch.utils.data import DataLoader
 from resnet32 import resnet32_normal as resnet32
 import argparse
 
-from random import shuffle
+import numpy as np
 
 
 parser = argparse.ArgumentParser(description='cifar10 classification models')
@@ -97,21 +97,23 @@ if __name__=='__main__':
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
-    dataset_train = CIFAR10(root='data', train=True, download=True, transform=transforms_train)
+    # there are 10 classes so the dataset name is cifar-10
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
     dataset_test = CIFAR10(root='data', train=False, download=True, transform=transforms_test)
     test_loader = DataLoader(dataset_test, batch_size=args.batch_size_test, shuffle=False, num_workers=args.num_worker)
 
-    # Split data
-    total_size = len(dataset_train)
-    split_sizes = [total_size // args.num_models for _ in range(args.num_models)]
-    split_sizes[-1] += total_size - sum(split_sizes)
-    indices = torch.randperm(total_size)
-    subsets = [Subset(dataset_train, indices[offset - size : offset]) for offset, size in zip(torch._utils._accumulate(split_sizes), split_sizes)]
-    train_loaders = [DataLoader(subset, batch_size=args.batch_size, shuffle=True, pin_memory=True, drop_last=True) for subset in subsets]
-    train_loaders = [[(data.to(device), targets.to(device)) for data, targets in dataloader] for dataloader in train_loaders]
+    dataset_train = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transforms_train)
 
-    # there are 10 classes so the dataset name is cifar-10
-    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    subsets = list(range(args.num_models))
+    subset_size = len(dataset_train) // args.num_models
+    
+    for i in range(10):
+        temp_idx = np.where(np.array(dataset_train.targets) == i)[0]
+        for j in range(args.num_models):
+            subsets[j] = np.concatenate((subsets[j], temp_idx[j*subset_size:(j+1)*subset_size]))
+    
+    train_loaders = [DataLoader(subset, batch_size=args.batch_size, shuffle=True, pin_memory=True, drop_last=True) for subset in subsets]
 
     print('==> Making model..')
 
@@ -144,8 +146,3 @@ if __name__=='__main__':
                 torch.save(state, 'pretrained_models/resnet32_cifar10_split_'+str(net_num)+'.pth')
 
         print(net_num+1, "training finished!")
-
-    # random model test
-    shuffle(net_list)
-    acc, loss = test(net_list[0], 0)
-    print("Random model accuracy:", acc, "loss:", loss)
